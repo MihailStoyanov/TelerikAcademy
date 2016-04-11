f459a74ec1944d51697b78810172aa36893ba918
Mihail Stoyanov <mihail@skysoft.bg>
2016-04-12 01:42:13 +0300

    C# Fundamentals - 02. Data types and variables Homework/C# Fundamentals Homework/C# Fundamentals - 02. Data types and variables

---
+++ Homework/C# Fundamentals Homework/C# Fundamentals - 02. Data types and variables/Data types and variables/14. Print the ASCII Table/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14.Print_the_ASCII_Table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.OutputEncoding = Encoding.ASCII;
+            for (int i = 33; i <= 126; i++)
+            {                                
+                Console.Write((char)i);
+            }
+            Console.WriteLine();
+        }
+    }
+}
