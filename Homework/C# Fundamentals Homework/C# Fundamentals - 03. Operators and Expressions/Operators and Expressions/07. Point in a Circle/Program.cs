c91b2921fe4be75bb086661acc5d728bea64306e
Mihail Stoyanov <mihail@skysoft.bg>
2016-04-12 05:02:41 +0300

    Retry

---
+++ Homework/C# Fundamentals Homework/C# Fundamentals - 03. Operators and Expressions/Operators and Expressions/07. Point in a Circle/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07.Point_in_a_Circle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = double.Parse(Console.ReadLine());
+            double y = double.Parse(Console.ReadLine());
+            double radius = 2.0;
+            double diagonal = Math.Sqrt( Math.Abs(x * x) + Math.Abs(y * y));
+
+            if (diagonal <= radius)
+            {
+                Console.WriteLine("yes {0:f2}", Math.Abs(diagonal));
+            }
+            else
+            {
+                Console.WriteLine("no {0:f2}", Math.Abs(diagonal));
+            }
+        }
+    }
+}
---
+++ Homework/C# Fundamentals Homework/C# Fundamentals - 03. Operators and Expressions/Operators and Expressions/08. Prime Check/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08.Prime_Check
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+
+
+
+            if (N >= 2)
+            {
+                if (N == 2)
+                {
+                    Console.WriteLine("true");
+                }
+                else
+                {
+                    for (int i = 2; i < N; i++)
+                    {
+                        if (N % i != 0 && N % i == 0)
+                        {
+                            Console.WriteLine("true");
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine("false");
+                            break;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("false");
+            }
+
+        }
+    }
+}
